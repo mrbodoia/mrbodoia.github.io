0481ddb784ad1d47e8e570146d908ac6ac89320d
Max Bodoia <max@bodoia.com>
2016-09-25 15:22:52 -0700

    cleaned up unnecessary parts

--- js/main.js
+++ js/main.js
@@ -1,100 +1,51 @@
-
 function main() {
-
-(function () {
-   'use strict';
-   
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
-        }
-      });
-
-	// affix the navbar after scroll below header
-$('#nav').affix({
-      offset: {
-        top: $('header').height()
-      }
-});	
-
-	// skills chart
-	$(document).ready(function(e) {
-	//var windowBottom = $(window).height();
-	var index=0;
-	$(document).scroll(function(){
-		var top = $('#skills').height()-$(window).scrollTop();
-		console.log(top)
-		if(top<-300){
-			if(index==0){	
-			
-				$('.chart').easyPieChart({
-					easing: 'easeOutBounce',
-					onStep: function(from, to, percent) {
-						$(this.el).find('.percent').text(Math.round(percent));
-					}
-				});
-			
+	(function () {
+		'use strict';
+		 
+		$('a.page-scroll').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+					scrollTop: target.offset().top - 40
+					}, 900);
+					return false;
 				}
-			index++;
-		}
-	})
-	//console.log(nagativeValue)
-	});
+			}
+		});
 
-
-  	// Portfolio isotope filter
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-
-    });
+		// Show Menu on Book
+		$(window).bind('scroll', function() {
+			var navHeight = $(window).height() - 500;
+			if ($(window).scrollTop() > navHeight) {
+				$('.navbar-default').addClass('on');
+			} else {
+				$('.navbar-default').removeClass('on');
+			}
+		});
+			
+		$('body').scrollspy({ 
+			target: '.navbar-default',
+			offset: 80
+		});
 	
-	  	
-    // CounterUp
-	$(document).ready(function( $ ) {
-		if($("span.count").length > 0){	
-			$('span.count').counterUp({
-					delay: 10, // the delay time in ms
-			time: 1500 // the speed time in ms
-			});
-		}
-	});
-	
-  	// Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});	
+		// Hide nav on click
+		$(".navbar-nav li a").click(function (event) {
+			// check if window is small enough so dropdown is created
+			var toggle = $(".navbar-toggle").is(":visible");
+			if (toggle) {
+				$(".navbar-collapse").collapse('hide');
+			}
+		});
 
-}());
-
-
+		// affix the navbar after scroll below header
+		$('#nav').affix({
+			offset: {
+				top: $('header').height()
+			}
+		});	
+	}());
 }
+
 main();
